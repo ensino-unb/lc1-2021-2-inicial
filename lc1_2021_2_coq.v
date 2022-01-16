8d272ce45d5743703221b589bf807e687fa9e4cb
Flávio Leonardo Cavalcanti de Moura <contato@flaviomoura.mat.br>
2022-01-15 22:12:12 -0300

    Add files via upload

---
+++ lc1_2021_2_coq.v
@@ -0,0 +1,11 @@
+(** * O assistente de provas Coq *)
+
+(**
+   Detalhar como foi o processo de instalação do sistema.
+*)
+
+(** * A Lógica Proposicional *)
+
+(**
+   A Lógica Proposicional (LP) ...
+*)
